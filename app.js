4b917c10d52b344000680c4b4b3c40a396b2b943
achalcvm1234-maker <achalcvm1234@gmail.com>
2026-03-30 14:45:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const supabaseKey = "sb_publishable_Rv9FEq22N2CpG2ccq5gQhg_LPtx5eYV"
 
 const client = createClient(supabaseUrl, supabaseKey)
 
+
 // Signup
 async function signup(){
 
@@ -20,6 +21,37 @@ if(error){
 alert(error.message)
 }else{
 alert("Signup successful")
+window.location="login.html"
 }
+
+}
+
+
+// Login
+async function login(){
+
+let email=document.getElementById("login_email").value
+let password=document.getElementById("login_password").value
+
+const { data, error } = await client.auth.signInWithPassword({
+email: email,
+password: password
+})
+
+if(error){
+alert(error.message)
+}else{
+alert("Login successful")
+window.location="dashboard.html"
+}
+
+}
+
+
+// Logout
+async function logout(){
+
+await client.auth.signOut()
+window.location="login.html"
 
 }
