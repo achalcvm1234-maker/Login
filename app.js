d31ebbb16fa789878b686f1a4748a89e23509763
achalcvm1234-maker <achalcvm1234@gmail.com>
2026-03-30 14:27:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,77 +1,25 @@
-const supabaseUrl="https://wdpfxnvhkyjfcvfvmjpt.supabase.co"
+const { createClient } = supabase
 
-const supabaseKey="sb_publishable_Rv9FEq22N2CpG2ccq5gQhg_LPtx5eYV"
+const supabaseUrl = "https://wdpfxnvhkyjfcvfvmjpt.supabase.co"
+const supabaseKey = "sb_publishable_Rv9FEq22N2CpG2ccq5gQhg_LPtx5eYV"
 
-const supabase=window.supabase.createClient(supabaseUrl,supabaseKey)
-
-
+const client = createClient(supabaseUrl, supabaseKey)
 
 // Signup
-
 async function signup(){
 
 let email=document.getElementById("email").value
-
 let password=document.getElementById("password").value
 
-const { data, error } = await supabase.auth.signUp({
-
+const { data, error } = await client.auth.signUp({
 email: email,
-
 password: password
-
 })
 
 if(error){
-
 alert(error.message)
-
 }else{
-
 alert("Signup successful")
-
 }
-
-}
-
-
-
-// Login
-
-async function login(){
-
-let email=document.getElementById("login_email").value
-
-let password=document.getElementById("login_password").value
-
-const { data, error } = await supabase.auth.signInWithPassword({
-
-email: email,
-
-password: password
-
-})
-
-if(error){
-
-alert("Login failed")
-
-}else{
-
-window.location="dashboard.html"
-
-}
-
-}
-
-
-
-// Logout
-
-async function logout(){
-
-await supabase.auth.signOut()
-
-window.location="login.html"
 
 }
